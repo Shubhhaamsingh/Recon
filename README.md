30c055347348ce5196c80f7695643b5436be3b2b
Shubham Singh <73239204+Shubhhaamsingh@users.noreply.github.com>
2022-02-28 17:50:29 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+ 
+
+
+# Recon : 
+**Recon** is a simple python tool that automates the process of Google Based Recon AKA ***Google Dorking*** .
+
+Current Version Run Google Search Queries to find :
+* Subdomains
+* Sub-Subdomains
+* Signup/Login pages
+* Dir Listing
+* Exposed Docs
+  - pdf...xls...docx...
+* WordPress Entries
+* Pasting Sites
+
+  
+
+## Installation :
+
+Use the package manager pip to install requirements
+
+```bash
+cd Recon
+python3 -m pip install -r requirements.txt
+python3 recon.py
+```
+
+ 
---
+++ plugins/pasting.py
@@ -0,0 +1,35 @@
+#Recon : Google Recon V1.0
+#Coded by Shubham SIngh
+#Github : https://www.github.com/Shubhhaamsingh/Recon
+
+
+import os 
+import sys
+import time
+import requests 
+import random 
+from googlesearch import search
+from termcolor import colored, cprint
+from http import cookiejar 
+
+TLD = ["co.ma","dz","ru","ca"]
+zolo  = random.choice(TLD)
+
+X = requests.get('https://www.google.com/webhp?ei=1') # : /
+X.status_code 
+if X.status_code == 200:
+ time.sleep(5)  
+
+f = open('grecon.config', 'r')
+alpha = f.read() 
+key = alpha
+time.sleep(5) 
+print(colored ('[>] Looking in Pasting Sites...' ,'green')) #Pasting Sites e.g : PasteBin...
+query = "site:pastebin.com | site:hastebin.com | site:carbon.now.sh " + key 
+for gamma in search(query, tld=zolo, num=30 , stop=60 , pause=2): 
+    print("" + gamma) 
+print ("") 
+if os.path.exists(".google-cookie"):
+ os.remove(".google-cookie")
+os.remove("grecon.config") 
+print(colored ('[>] Done...Happy Hunting' ,'green'))
